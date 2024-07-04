ecbe4b97f4a94aedb3233f19e6ca1275d105b151
yanfi-sher <loss.soul20@gmail.com>
2024-07-04 19:47:40 +0300

    1. delete main.java 2. add methods to Calculations.java 3. add tests to all methods 4. change a little pom.xml

---
+++ src/main/java/demo/Calculations.java
@@ -0,0 +1,51 @@
+package demo;
+
+public class Calculations {
+
+    public static int sum(int op1, int op2){
+        int res = op1+op2;
+        return res;
+    }
+
+    public static int sub(int op1, int op2){
+        int res = op1-op2;
+        return res;
+    }
+
+    public static int divide(int op1, int op2){
+        return op1/op2;
+    }
+
+    public static int multiply(int op1, int op2){
+        return op1*op2;
+    }
+
+    public static int sumOfDigits(int number) {
+        int res = 0;
+        while (number != 0) {
+            res += number % 10;
+            number /= 10;
+        }
+        return res;
+    }
+
+    public static int maxDigit(int number){
+        int res = 0;
+        number = Math.abs(number);
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit > res) {
+                res = digit;
+            }
+            number /= 10;
+        }
+        return res;
+    }
+
+    public static boolean isDivideOn(int number, int dividor) {
+        return number % dividor == 0;
+    }
+
+    
+
+}
---
+++ src/test/java/demo/CalculationsTest.java
@@ -0,0 +1,68 @@
+package demo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import static demo.Calculations.divide;
+import static demo.Calculations.isDivideOn;
+import static demo.Calculations.maxDigit;
+import static demo.Calculations.multiply;
+import static demo.Calculations.sub;
+import static demo.Calculations.sum;
+
+public class CalculationsTest {
+
+    @Test
+    void sumTest(){
+        assertEquals(4, sum(1, 3));
+        assertNotEquals(-4, sum(-2, 2));
+    }
+
+    @Test
+    void subTest(){
+        assertEquals(4, sub(7, 3));
+        assertNotEquals(4, sub(-2, 2));
+    }
+
+    @Test
+    void divTest(){
+        assertEquals(4, divide(12, 3));
+        assertNotEquals(4, divide(-2, 2));
+    }
+
+    @Test
+    void multiplyTest(){
+        assertEquals(36, multiply(12, 3));
+        assertNotEquals(4, multiply(-2, 2));
+    }
+
+    @Test
+    void sumOfDigitsTest() {
+        assertEquals(10, Calculations.sumOfDigits(1234));
+        assertEquals(0, Calculations.sumOfDigits(0));
+        assertEquals(9, Calculations.sumOfDigits(9));
+    }
+
+    @Test
+    void maxDigitTest() {
+        assertEquals(5, maxDigit(12345));
+        assertEquals(9, maxDigit(9876));
+        assertEquals(0, maxDigit(0));
+        assertEquals(8, maxDigit(-2468));
+        assertEquals(5, maxDigit(555555));
+    }
+
+    @Test
+    void isDivideOnTest() {
+        assertTrue(isDivideOn(10, 2));
+        assertFalse(isDivideOn(10, 3));
+        assertTrue(isDivideOn(15, 5));
+        assertFalse(isDivideOn(17, 4));
+        assertTrue(isDivideOn(0, 1));
+    }
+
+}
